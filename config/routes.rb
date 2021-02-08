d027206a44fbc873cb9e46c6ed3521489d0aeed3
Alexandre Platteeuw <aplatteeuw@outlook.fr>
2021-02-08 20:21:49 +0100

    create twitter accounts and tweets

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,9 @@ Rails.application.routes.draw do
   patch '/password/reset/edit', to: 'password_resets#update'
 
   get 'auth/twitter/callback', to: "omniauth_callbacks#twitter"
+
+  post 'auth/twitter', as: 'connect_twitter_account'
+
+  resources :twitter_accounts
+  resources :tweets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_171005) do
+ActiveRecord::Schema.define(version: 2021_02_08_165556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2021_02_06_171005) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "twitter_account_id", null: false
+    t.text "body"
+    t.datetime "publish_at"
+    t.string "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["twitter_account_id"], name: "index_tweets_on_twitter_account_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "twitter_accounts", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -64,5 +76,7 @@ ActiveRecord::Schema.define(version: 2021_02_06_171005) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tweets", "twitter_accounts"
+  add_foreign_key "tweets", "users"
   add_foreign_key "twitter_accounts", "users"
 end
